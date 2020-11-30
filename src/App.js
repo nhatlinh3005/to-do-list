8fb460516fda746771faed451d0792899cb0262c
unknown <nhatlinh3005@gmail.com>
2020-12-01 00:15:05 +0700

    start project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="container">
+            <div className="title">
+				<h1>Project 01 - ToDo List  ReactJS</h1>
+			</div>
+        </div>
+    );
+}
+
+export default App;
